58afdfb79794c5cf1c3d70f62d3fcec38ea595ec
Paulo Campos Filho <pezaum@gmail.com>
2024-03-27 15:55:06 -0700

    Add permission check setting for bot to interact only with trusted members.

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,18 +1,26 @@
 const { Events } = require('discord.js');
 const handleMessageResponse = require('../helpers/handleMessageResponse');
 const theWiseTreeUserId = process.env.CLIENT_ID;
+const botRepliesOnlyToTrusted = Boolean(process.env.BOT_REPLIES_ONLY_TO_TRUSTED);
+const slashCommandRolePermissions = require('../constants/slashCommandRolePermissions');
 
 module.exports = {
   name: Events.MessageCreate,
   async execute(msg) {
     try {
       const client = msg.client;
-      const messageResponse = await client.messageResponses.get(msg.content);
+      const member = msg.member;
+      const isTrustedMember = !!member.roles.cache.get(slashCommandRolePermissions.trusted_role);
+      const messageResponse = await (client.messageResponses.get(msg.content));
+      const botCanTrigger = messageResponse?.botCanTrigger || false;
+
       // message does not trigger a bot response.
       if (!messageResponse) return;
-      const botCanTrigger = messageResponse.botCanTrigger || false;
+      // if bot is set to only respond to trusted members, don't respond to untrusted.
+      if (botRepliesOnlyToTrusted && !isTrustedMember) return;
       // do not respond.
       if (msg.author.id === theWiseTreeUserId && !botCanTrigger) return;
+
       handleMessageResponse(msg, messageResponse);
     }
     catch (e) {

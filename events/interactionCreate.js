016b401194c8cb16ee9dc720fb32fb2244b5be52
Paulo Campos Filho <pezaum@gmail.com>
2024-04-27 23:11:47 -0700

    Add comments

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -3,6 +3,7 @@ const { Events } = require('discord.js');
 module.exports = {
   name: Events.InteractionCreate,
   async execute(interaction) {
+    // TODO: REFACTOR this function. Too much repetition.
     if (interaction.isChatInputCommand()) {
 
       const command = interaction.client.commands.get(interaction.commandName);
@@ -31,6 +32,7 @@ module.exports = {
       const commandButton = interaction.client.commands.get(commandName);
       commandButton.responseHandler(interaction);
     }
+    // UserContextCommand interaction
     if (interaction.isUserContextMenuCommand()) {
       const commandName = interaction.commandName;
       const command = interaction.client.userContextCommands.get(commandName);

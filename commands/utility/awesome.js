64fd4477789fe1cf11dc104eca4b1f3ae8a07768
Paulo Campos Filho <pezaum@gmail.com>
2024-06-27 12:16:20 -0700

    Add personalization/signature to awesome response

--- commands/utility/awesome.js
+++ commands/utility/awesome.js
@@ -1,21 +1,23 @@
 const { SlashCommandBuilder } = require('discord.js');
 const slashCommandRolePermissions = require('../../constants/slashCommandRolePermissions');
+const { toCursive } = require('../../helpers/toCursive');
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('awesome')
-		.setDescription('Tell Paul he is AWESOME!'),		
-	async execute(interaction) {
-		try {
-			const theMessage = `Hey Paul-\nThanks so much!\nYOU ARE AWESOME!!! ☺\n\nHave a great weekend.\n\nThank you,\nDonna`
-			await interaction.reply({ content: 'You speak with my voice', ephemeral: true });
-			await interaction.channel.send({ content: theMessage });
-		}
-		catch (e) {
-			console.log(e);
-		}
-	},
-	memberCanExecute(member) {
-		return !!member.roles.cache.get(this.roleRequired);
-	},
-	roleRequired: slashCommandRolePermissions.admin,
+  data: new SlashCommandBuilder()
+    .setName('awesome')
+    .setDescription('Tell Paul he is AWESOME!'),
+  async execute(interaction) {
+    try {
+      const sender = interaction.member.nickname || interaction.member.user.globalName;
+      const theMessage = `Hey Paul-\nThanks so much!\nYOU ARE AWESOME!!! ☺\n\nHave a great weekend.\n\nThank you,\n${toCursive(sender)}`
+      await interaction.reply({ content: 'You speak with my voice', ephemeral: true });
+      await interaction.channel.send({ content: theMessage });
+    }
+    catch (e) {
+      console.log(e);
+    }
+  },
+  memberCanExecute(member) {
+    return !!member.roles.cache.get(this.roleRequired);
+  },
+  roleRequired: slashCommandRolePermissions.admin,
 };
